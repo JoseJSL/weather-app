8f77084bf1ac092d7e41b1d658c9cbcd028dc250
JoseJSL <josehyvy2@gmail.com>
2021-10-26 00:05:04 -0600

    terminado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  wtrApiUrl: "https://api.openweathermap.org/data/2.5/",
+  wtrApiKey: "a41c76b7bf7a92a6a79087b88fdefa1d"
 };
+
+
